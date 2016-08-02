2d9eb6909a3c3e178630c86df32e398be859b20d
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2016-08-02 18:34:45 +0600

    Some fixes

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -9,20 +9,3 @@ export default ({filter, children}) => (
 		activeClassName='acitve-tab'
 	>{children}</Link>
 );
-
-
-
-// export default class FilterButton extends Component {
-// 	onFilterClick(e) {
-// 		this.props.newFilter(e.target.innerText);
-// 	}
-
-// 	render() {
-// 		return (
-// 			<Link className={this.props.filter.isActive ? 'header-item acitve-tab' : 'header-item'} 
-// 				onClick={::this.onFilterClick}>
-// 				{this.props.filter.name}
-// 			</Link>
-// 		)
-// 	}
-// }
